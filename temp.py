8c877e181958fcee21af79040e352413f8881a17
HungDong <narutodjh@gmail.com>
2024-04-22 19:15:20 +0700

    udpate README

--- temp.py
+++ temp.py
@@ -6,7 +6,9 @@ import KeyFunction
 private_key = KeyFunction.get_private_key()
 if (private_key == None):
     print("Error: Private key is None")
+    print("Creating private key ...")
     public_key, private_key = KeyFunction.generate_key()
+    print("Private key created")
 else:
     print("Private key exists")
     public_key = KeyFunction.get_public_key()
